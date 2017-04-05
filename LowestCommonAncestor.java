8721b36d49fce4472d110cd9e6d9c1d7cbe85e11
vyerneni17 <venkata.yerneni17@gmail.com>
2017-04-04 23:48:21 -0700

    Lowest Common Ancestor

--- LowestCommonAncestor.java
+++ LowestCommonAncestor.java
@@ -1,31 +1,24 @@
-/*
-  The Node class is defined as follows:
-    class Node {
-        int data;
-        Node left;
-        Node right;
-     }
-
-     Input Format
-     5          //number of elements in the BST
-     1 2 3 4 5  //elements in the tree in insertion order
-     3 5        //Two elements you need to find the LCA for
+/* Node is defined as :
+ class Node
+    int data;
+    Node left;
+    Node right;
 */
 
-    boolean checkBST(Node root) {
-        return isBST(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+static Node lca(Node root,int v1,int v2)
+    {
+        if((root.data>v1 && root.data<v2) || (root.data<v1 && root.data>v2) || root.data==v1 || root.data==v2){
+            return root;
+        }
+
+        else{
+            Node temp = root;
+            if(temp.data > v1){
+                temp = lca(temp.left,v1,v2);
+            }
+            else{
+                temp = lca(temp.right,v1,v2);
+            }
+        }
+    return root;
     }
-
-
-    public boolean isBST(Node root, int min, int max) {
-      if(root == null) {
-        return true;
-      }
-
-    if(root.data >= max || root.data <= min) {
-      return false;
-    }
-    
-    return (isBST(root.left, min, root.data) && isBST(root.right, root.data, max));
-  }
-}
